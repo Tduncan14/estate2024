8ae6038320178fd001ca3772c5a0c55d99161f6f
tduncan14 <nbatre1@yahoo.com>
2024-10-08 05:06:28 -0700

    keep moving forward

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './layout.scss'
+
+
+function App() {
+  return (
+    <div className="layout">
+      hello again
+    </div>
+  );
+}
+
+export default App;
