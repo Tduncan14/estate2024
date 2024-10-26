7cda1f96fb63e3732639186b4608c8e7b0b3a0a7
tduncan14 <nbatre1@yahoo.com>
2024-10-25 20:21:25 -0700

    keep moving forward

--- estates/src/components/Card/Card.js
+++ estates/src/components/Card/Card.js
@@ -2,6 +2,11 @@ import './Card.scss';
 import { Link } from 'react-router-dom';
 import React from 'react'
 import pin from '../../images/pin.png';
+import bed from '../../images/bed.png';
+import bath from '../../images/bath.png';
+import save from '../../images/save.png';
+import chat from '../../images/chat.png'
+
 
 const Card = ({ item }) => {
     return (
@@ -23,6 +28,34 @@ const Card = ({ item }) => {
                     <img src={pin} />
                     <span>{item.address}</span>
                 </p>
+
+                <p className="price">${item.price}</p>
+
+                <div className="bottom">
+                    <div className="features">
+                        <div className="feature">
+                            <img src={bed} alt="" />
+                            <span>{item.bedroom} bedroom</span>
+
+                        </div>
+
+
+                        <div className="feature">
+                            <img src={bath} alt="" />
+                            <span>{item.bathroom} bathroom</span>
+
+                        </div>
+                    </div>
+                    <div className="icons">
+                        <div className="icon">
+                            <img src={save} alt="" />
+                        </div>
+
+                        <div className="icon">
+                            <img src={chat} alt="" />
+                        </div>
+                    </div>
+                </div>
 
             </div>
 
