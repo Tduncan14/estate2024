9adeb133c71bd6f9a2f7e8120bb90f3a8a1e019b
tduncan14 <nbatre1@yahoo.com>
2024-10-25 19:48:15 -0700

    keep moving forward

--- estates/src/components/Card/Card.js
+++ estates/src/components/Card/Card.js
@@ -1,10 +1,32 @@
 import './Card.scss';
+import { Link } from 'react-router-dom';
 import React from 'react'
+import pin from '../../images/pin.png';
 
 const Card = ({ item }) => {
     return (
-        <div>
-            hey
+        <div class="card">
+
+            <Link to={`/${item.id}`} className="imageContainer">
+                <img src={item.img} alt="properties" />
+
+            </Link>
+
+
+            <div className="textContainer">
+                <h2 className="title">
+                    <Link to={`/${item.id}`}>{item.title}</Link>
+                </h2>
+
+
+                <p className="address">
+                    <img src={pin} />
+                    <span>{item.address}</span>
+                </p>
+
+            </div>
+
+
         </div>
     )
 }
