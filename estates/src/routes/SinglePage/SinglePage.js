3f552c0de30d4eca2f813da637fd19a9ac79260a
tduncan14 <nbatre1@yahoo.com>
2024-11-02 20:30:34 -0700

    keep moving forward

--- estates/src/routes/SinglePage/SinglePage.js
+++ estates/src/routes/SinglePage/SinglePage.js
@@ -2,18 +2,28 @@ import './SinglePage.scss';
 import Slider from '../../components/slider/Slider';
 import { singlePostData, userData } from '../../lib/dummydata';
 import Pin from '../../images/pin.png';
+import chat from '../../images/chat.png';
+import save from '../../images/save.png';
+import utility from '../../images/utility.png';
+import pet from '../../images/pet.png';
+import bath from '../../images/bath.png';
+import bed from '../../images/bed.png';
+import size from '../../images/size.png';
+import bus from '../../images/bus.png';
+import restarunt from '../../images/restaurant.png';
+import school from '../../images/school.png';
+import Map from '../../components/Map/Map';
+
 
 
 
 import React from 'react'
-
-const SinglePage = () => {
+function SinglePage() {
     return (
         <div className="singlePage">
             <div className="details">
                 <div className="wrapper">
                     <Slider images={singlePostData.images} />
-
                     <div className="info">
                         <div className="top">
                             <div className="post">
@@ -22,30 +32,99 @@ const SinglePage = () => {
                                     <img src={Pin} alt="" />
                                     <span>{singlePostData.address}</span>
                                 </div>
-
-                                <div className="price">
-                                    {singlePostData.price}
-                                </div>
+                                <div className="price">$ {singlePostData.price}</div>
                             </div>
                             <div className="user">
                                 <img src={userData.img} alt="" />
-                                <span>{userData.name}</span>
                             </div>
                         </div>
-                        <div className="bottom">
-                            {singlePostData.description}
-                        </div>
+                        <div className="bottom">{singlePostData.description}</div>
                     </div>
                 </div>
             </div>
             <div className="features">
                 <div className="wrapper">
-
+                    <p className="title">General</p>
+                    <div className="listVertical">
+                        <div className="feature">
+                            <img src={utility} alt="" />
+                            <div className="featureText">
+                                <span>Utilities</span>
+                                <p>Renter is responsible</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <img src={pet} alt="" />
+                            <div className="featureText">
+                                <span>Pet Policy</span>
+                                <p>Pets Allowed</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <img src={size} alt="" />
+                            <div className="featureText">
+                                <span>Property Fees</span>
+                                <p>Must have 3x the rent in total household income</p>
+                            </div>
+                        </div>
+                    </div>
+                    <p className="title">Sizes</p>
+                    <div className="sizes">
+                        <div className="size">
+                            <img src={size} alt="" />
+                            <span>80 sqft</span>
+                        </div>
+                        <div className="size">
+                            <img src={bed} alt="" />
+                            <span>2 beds</span>
+                        </div>
+                        <div className="size">
+                            <img src={bath} alt="" />
+                            <span>1 bathroom</span>
+                        </div>
+                    </div>
+                    <p className="title">Nearby Places</p>
+                    <div className="listHorizontal">
+                        <div className="feature">
+                            <img src={school} alt="" />
+                            <div className="featureText">
+                                <span>School</span>
+                                <p>250m away</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <img src={pet} alt="" />
+                            <div className="featureText">
+                                <span>Bus Stop</span>
+                                <p>100m away</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <img src={restarunt} alt="" />
+                            <div className="featureText">
+                                <span>Restaurant</span>
+                                <p>200m away</p>
+                            </div>
+                        </div>
+                    </div>
+                    <p className="title">Location</p>
+                    <div className="mapContainer">
+                        <Map items={[singlePostData]} />
+                    </div>
+                    <div className="buttons">
+                        <button>
+                            <img src={chat} alt="" />
+                            Send a Message
+                        </button>
+                        <button>
+                            <img src={save} alt="" />
+                            Save the Place
+                        </button>
+                    </div>
                 </div>
             </div>
-
         </div>
-    )
+    );
 }
 
 export default SinglePage
